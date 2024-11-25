7b6f2bc9835d61de4478653896252697b96e4c85
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-11-25 15:22:56 -0800

    refactor(search suggestions): rename equipmentLinkId search params to linkHashId

--- src/scripts/suggestions.ts
+++ src/scripts/suggestions.ts
@@ -233,7 +233,7 @@ export function suggestionsInputHandler(input: HTMLInputElement) {
           const templateContent = itemTemplate.content.cloneNode(true);
 
           const link = templateContent.firstChild as HTMLAnchorElement;
-          link.href += `?equipmentLinkId=${resolvedItem.id}`;
+          link.href += `?linkHashId=${resolvedItem.id}`;
           link.textContent = resolvedItem.title;
 
           menuContainer.appendChild(link);
